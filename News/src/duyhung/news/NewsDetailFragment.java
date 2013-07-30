e509a1082b0b3dadf1d45fb42fcce9fd6351127e
Duyhung <duyhung.1no2@gmail.com>
2013-07-30 21:26:04 +0700

    Dang lam do? > Web View... NeXT>>>

--- News/src/duyhung/news/NewsDetailFragment.java
+++ News/src/duyhung/news/NewsDetailFragment.java
@@ -1,5 +1,6 @@
 package duyhung.news;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -11,36 +12,110 @@ public class NewsDetailFragment extends Activity {
 
 	private WebView newsWebView;
 	private ProgressDialog progressDialog;
-
+	
+/*	private static String FILENAME = "";
+	private static final String OUTPUT_FILENAME = "NEWS_OUTPUT.html";
+	private static String OUTPUT = "<html>" +
+												"<head>" +
+													"<script src=\"http://code.jquery.com/jquery-1.9.1.js\"></script>" +
+													"<script>$(document).ready(function(){" +
+														"$('.spanTime').load('" + FILENAME + " .spanTime');" +
+														"$('.title').load('" + FILENAME + " .title');" +
+														"$('.fck_detail').load('" + FILENAME + " .fck_detail');});" +
+													"</script>" +
+												"</head>" +
+												"<body>" +
+													"<div class=\"title\"></div>" +
+													"<div class=\"spanTime\" style=\"font-style:italic;\"></div>" +
+													"<div class=\"fck_detail\"></div>" +
+												"</body>" +
+											"</html>"; 
+*/
 	@Override
+	@SuppressLint("SetJavaScriptEnabled")
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_news_detail);
 
 		String newsLink = getIntent().getExtras().getString(Variables.LINK);
-		
+
 		newsWebView = (WebView) findViewById(R.id.newsWebView);
-//		newsWebView.getSettings().setSupportZoom(true);
-//		newsWebView.setInitialScale(1);
-//		newsWebView.getSettings().setLoadWithOverviewMode(true);
-//		newsWebView.getSettings().setUseWideViewPort(true);
-//		newsWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-//		newsWebView.setScrollbarFadingEnabled(false);
-		
+		newsWebView.getSettings().setAllowFileAccess(true);
+		newsWebView.getSettings().setJavaScriptEnabled(true);
+
 		progressDialog = ProgressDialog.show(this, "", "Loading news ... ");
-		
+
+/*		writeOutputFile();
+		writeNewsFile(newsLink);
+*/
 		newsWebView.setWebViewClient(new NewsWebViewClient());
 		newsWebView.loadUrl(newsLink);
+//		newsWebView.loadUrl("file:///" + Environment.getExternalStorageDirectory() + "/" + OUTPUT_FILENAME);
 	}
 
-	private class NewsWebViewClient extends WebViewClient{
+/*	private void writeNewsFile(String newsLink) {
+		FILENAME = Uri.parse(newsLink).getLastPathSegment();
+		File file = new File(Environment.getExternalStorageDirectory() + "/", FILENAME);
+		try {
+			if (!file.exists())
+				file.createNewFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		ByteArrayOutputStream bais = new ByteArrayOutputStream();
+		InputStream inStream = null;
+		URL url = null;
+		try {
+			url = new URL(newsLink);
+
+			inStream = url.openStream();
+			byte[] byteChunk = new byte[4096];
+			int n;
+
+			while ((n = inStream.read(byteChunk)) > 0) {
+				bais.write(byteChunk, 0, n);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (inStream != null) {
+				try {
+					inStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		try {
+			FileOutputStream outStream = new FileOutputStream(file);
+			outStream.write(bais.toByteArray());
+			outStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeOutputFile() {
+		File f = new File(Environment.getExternalStorageDirectory() + "/" + OUTPUT_FILENAME);
+		try {
+			FileOutputStream outStream = new FileOutputStream(f);
+			outStream.write(OUTPUT.getBytes());
+			outStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+*/
+	private class NewsWebViewClient extends WebViewClient {
 		@Override
 		public void onPageFinished(WebView view, String url) {
-			if(progressDialog != null){
+			if (progressDialog != null) {
 				progressDialog.dismiss();
 			}
 			super.onPageFinished(view, url);
 		}
 	}
-	
+
 }
