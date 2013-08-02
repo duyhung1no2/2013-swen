42137aaaaa87a68c17242a95110820a93c6b6c09
Duyhung <duyhung.1no2@gmail.com>
2013-08-03 00:14:16 +0700

    Done > Load Section of Webspage, using HtmlCleaner & XPath.

--- News/src/duyhung/news/NewsFragment.java
+++ News/src/duyhung/news/NewsFragment.java
@@ -1,6 +1,14 @@
 package duyhung.news;
 
+import java.net.URL;
+
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.SimpleHtmlSerializer;
+import org.htmlcleaner.TagNode;
+
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,7 +22,7 @@ public class NewsFragment extends Fragment {
 
 	private WebView newsWebView;
 	private ProgressDialog progressDialog;
-	
+
 	public static final String IS_SELECTED = "IS_SELECTED";
 	private String newsLink;
 
@@ -22,9 +30,12 @@ public class NewsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_news, container, false);
 		newsWebView = (WebView) v.findViewById(R.id.newsWebView);
+		newsWebView.getSettings().setDefaultTextEncodingName("utf-8");
+		newsWebView.getSettings().setAllowContentAccess(true);
+
 		newsLink = getArguments().getString(Variables.LINK);
 
-		if(getArguments().getBoolean(IS_SELECTED)){
+		if (getArguments().getBoolean(IS_SELECTED)) {
 			retrieveContent();
 		}
 
@@ -34,12 +45,10 @@ public class NewsFragment extends Fragment {
 	public void retrieveContent() {
 		if (isAdded()) {
 			progressDialog = ProgressDialog.show(getActivity(), "", "Đang tải nội dung ... ");
-			newsWebView.setWebViewClient(new NewsWebViewClient());
-			newsWebView.loadUrl(newsLink);
-
+			new LoadWebTask().execute(newsLink);
 		}
 	}
-	
+
 	private class NewsWebViewClient extends WebViewClient {
 		@Override
 		public void onPageFinished(WebView view, String url) {
@@ -49,29 +58,47 @@ public class NewsFragment extends Fragment {
 			super.onPageFinished(view, url);
 		}
 	}
-	
-//	private class LoadWebTask extends AsyncTask<String, Void, Void> {
-//		@Override
-//		protected Void doInBackground(String... urls) {
-//			HtmlCleaner cleaner = new HtmlCleaner();
-//			TagNode node = null;
-//			String content = "";
-//			try {
-//				node = cleaner.clean(new URL(urls[0]).openStream());
-//				
-//				Object[] contentNode = node.evaluateXPath("//html/body/div[id='wrap']/div[id='content']/div[class='content-center']/div[class='content']/div[class='cxtLeft']/div[class='fck_detail']/");
-//				for (int i = 0; i < contentNode.length; i++) {
-////					NodeList childNode = contentNode
-////							item(i).getChildNodes();
-////					content += contentNode[i].get
-//				}
-//						
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//
-//			return null;
-//		}
-//	}
+
+	private class LoadWebTask extends AsyncTask<String, Void, Void> {
+
+		String content = "";
+
+		@Override
+		protected Void doInBackground(String... urls) {
+
+			HtmlCleaner cleaner = new HtmlCleaner();
+			CleanerProperties props = cleaner.getProperties();
+			props.setAllowHtmlInsideAttributes(true);
+			props.setAllowMultiWordAttributes(true);
+			props.setRecognizeUnicodeChars(true);
+			props.setOmitComments(true);
+
+			try {
+				TagNode rootNode = cleaner.clean(new URL(urls[0]).openStream());
+				SimpleHtmlSerializer serializer = new SimpleHtmlSerializer(props);
+				
+				Object[] titleNode = rootNode.evaluateXPath("//h1[@class='Title']");
+				content += serializer.getAsString((TagNode) titleNode[0]);
+				
+				Object[] dateNode = rootNode.evaluateXPath("//span[@class='spanTime']");
+				content += serializer.getAsString((TagNode) dateNode[0]);
+				
+				Object[] contentNode = rootNode.evaluateXPath("//div[@class='fck_detail']");
+				content += serializer.getAsString((TagNode) contentNode[0]);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			newsWebView.setWebViewClient(new NewsWebViewClient());
+			newsWebView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+		}
+	}
 
 }
